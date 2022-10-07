c93c3a46d3c2d3aafa5cd68366fd51c5b4d30082
Git-Jake-Hill <89761486+Git-Jake-Hill@users.noreply.github.com>
2022-10-07 21:09:00 +1100

    update

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,3 +1,28 @@
+import { useState, useEffect } from "react";
+
 export const AutoComplete = () => {
-    return ( <div>AutoComplete</div>)
+    const [company, setCompany] = useState("");
+
+    const submit = (e) => {
+        e.preventDefault();
+        alert(company);
+    }
+
+    return (
+        <div className="w-50 p-5 rounded mx-auto">
+            <div className="form-floating dropdown">
+                <input type="text" value={company} onChange={(event) => setCompany(event.target.value)} />
+            </div>
+            <form>
+            </form>
+        </div>)
 }
+
+// crate a input to search
+// input html element
+
+// onChange update search input
+// send fetch request to api for company names autocomplete
+
+// once company is selected
+// add it to the watchList to update the table data
